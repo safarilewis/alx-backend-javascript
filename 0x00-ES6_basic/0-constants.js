b064619dbe7be81ac435485beaf27f2cd2d4a5fb
safarilewis <lleewwii1@gmail.com>
2023-03-20 14:43:01 +0300

    fixed linter errors

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
-//Function to instantiate variables using const
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
-    return task;
+  const task = 'I prefer const when I can.';
+  return task;
 }
-//getLast function
-export function getLast() {
-    return ' is okay';
-}
-//Function to instatiate variables using let
-export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getLast();
 
-    return combination;
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }

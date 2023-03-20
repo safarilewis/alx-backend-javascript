975b6b6929c9e126b2db09fb6ae3bddc000ae0c8
safarilewis <lleewwii1@gmail.com>
2023-03-20 15:06:01 +0300

    fixed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
-export function taskBlock(trueorFalse) {
-  var task = false;
-  var task2 = true;
+export default function taskBlock(trueorFalse) {
+  const task = false;
+  const task2 = true;
 
   if (trueorFalse) {
     let task = true;
     let task2 = false;
   }
+
   return [task, task2];
 }

af9b48c3bc7a759ba21232dda7e60105d6dd687c
safarilewis <lleewwii1@gmail.com>
2023-03-20 12:55:27 +0300

    adding config files

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
+        "browser": false,
+        "es6": true,
+        "jest": true
     },
     "extends": "eslint:recommended",
     "overrides": [

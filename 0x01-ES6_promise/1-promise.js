1f737e7c876d61e1fbb59c88c1dffea83f5ece23
safarilewis <lleewwii1@gmail.com>
2023-03-24 22:04:42 +0300

    promise task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+export default function getResponseFromAPI(success) {
+  const p = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject("The fake API is not working currently");
+    }
+  });
+  p.catch((err)=>{
+    return err;
+  })
+  return p;
+}

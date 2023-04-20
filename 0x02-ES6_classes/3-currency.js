50b19eacf10398dfb08e3aa370141396181864c6
safarilewis <lleewwii1@gmail.com>
2023-04-20 11:58:28 +0300

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, string) {
+    this.code = code;
+    this.string = string;
+  }
+
+  set string(string) {
+    if (typeof string !== 'string') {
+      throw new TypeError('String must be string');
+    }
+    this._string = string;
+  }
+
+  get string() {
+    return this._string;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  displayFullCurrency() {
+    return (`${this._string} ${this._code}`);
+  }
+}

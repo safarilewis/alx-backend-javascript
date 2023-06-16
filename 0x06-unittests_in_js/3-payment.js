5c48b26a44ee99fd93c5b27933cf70172b5c2c33
safarilewis <lleewwii1@gmail.com>
2023-06-17 02:50:36 +0300

    fixing indentation

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
 const Utils = require("./utils");
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const answer = Utils.calculateNumber("SUM", totalAmount, totalShipping);
-    console.log(`The total is: ${answer}`);
+  const answer = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+  console.log(`The total is: ${answer}`);
 }
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,8 +1,7 @@
 const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
-    if (success) {
-      resolve({data: 'Successful response from the API'});
-    }
-  });
-  
-  module.exports = getPaymentTokenFromAPI;
-  
+  if (success) {
+    resolve({ data: 'Successful response from the API' });
+  }
+});
+
+module.exports = getPaymentTokenFromAPI;

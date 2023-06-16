0bbc71d388fe6732178139546ef8926b116f1f94
safarilewis <lleewwii1@gmail.com>
2023-06-16 23:08:28 +0300

    stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', function () {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+})
+process.on('end', function () {
+  process.stdout.write('This important software is now closing\n');
+})

ee83894db5d7538482f3f5b0bc0660d159d0c396
safarilewis <lleewwii1@gmail.com>
2023-06-19 15:47:48 +0300

    weak map

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = WeakMap();
 const MAX = 5;
 export default function queryAPI(endpoint) {
   // eslint-disable-next-line
-  !weakMap.has(endpoint) ? weakMap.set(endpoint, 0) : weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  !(weakMap.has(endpoint)) ? weakMap.set(endpoint, 0) : weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   if (weakMap.get(endpoint) >= MAX) {
     throw new Error('Endpoint load is high');
   }

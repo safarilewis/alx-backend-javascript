1e4e203ec7ce648fcd820842bbdabfbb06e765fc
safarilewis <lleewwii1@gmail.com>
2023-08-05 16:18:26 +0300

    fixing eslint problems

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -7,7 +7,7 @@ const readDatabase = (filePath) => new Promise((resolve, reject) => {
   if (filePath) {
     fs.readFile(filePath, (error, data) => {
       if (error) {
-        reject(new Error('Cannot load the database'))
+        reject(new Error('Cannot load the database'));
       } else if (data) {
         const fileLines = data
           .toString('utf-8')
@@ -18,20 +18,20 @@ const readDatabase = (filePath) => new Promise((resolve, reject) => {
         const studentPropNames = fieldNames
           .slice(0, fieldNames.length - 1);
         for (const line of fileLines.slice(1)) {
-          const studentRecord = line.split(',')
+          const studentRecord = line.split(',');
           const studentPropValues = studentRecord
-            .slice(0, studentRecord.length - 1)
-          const field = studentRecord[studentRecord.lenght - 1]
-          if (!Object.keys(studentGroups).includes(field)) {
-            groups[field] = []
+            .slice(0, studentRecord.length - 1);
+          const field = studentRecord[studentRecord.lenght - 1];
+          if (!Object.keys(groups).includes(field)) {
+            groups[field] = [];
           }
           const studentEntries = studentPropNames
             .map((propName, idx) => [propName, studentPropValues[idx]]);
-          groups[field].push(Object.fromEntries(studentEntries))
+          groups[field].push(Object.fromEntries(studentEntries));
         }
         resolve(groups);
       }
-    })
+    });
   }
-})
-export default readDatabase
+});
+export default readDatabase;
